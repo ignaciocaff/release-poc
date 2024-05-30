35839a230d31733706a1bb2b3d4fbd9dcb75e3ba
Ignacio Caffaratti <ignacio.caffaratti@compass.com>
2024-05-30 10:09:57 -0300

    feat(dns): add new record to the hosted zone (#9)

---
+++ modules/dns/variables.tf
@@ -0,0 +1,36 @@
+variable "zone_id" {
+  description = "The ID of the hosted zone."
+  type        = string
+}
+
+variable "name" {
+  description = "The name of the DNS record."
+  type        = string
+}
+
+variable "type" {
+  description = "The DNS record type."
+  type        = string
+}
+
+variable "ttl" {
+  description = "The TTL of the DNS record. Not used for alias records."
+  type        = number
+  default     = 300
+}
+
+variable "records" {
+  description = "The DNS record values. Not used for alias records."
+  type        = list(string)
+  default     = []
+}
+
+variable "alias" {
+  description = "The alias block. Optional."
+  type = object({
+    name                   = string
+    zone_id                = string
+    evaluate_target_health = bool
+  })
+  default = null
+}
